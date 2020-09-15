5078d450df1c3ff72481957001341562e0f0d39e
Metin Balaban <metin@Metins-MBP.san.rr.com>
2020-09-15 08:12:36 -0700

    decompose, alignment, contraint tree,bipartition alignment

---
+++ run_didactic.py
@@ -0,0 +1,266 @@
+import json
+from collections import deque
+
+from compute_bipartition_alignment import compute_bipartition_alignment
+from fasta2dic import fasta2dic
+from newick_extended import read_tree_newick
+import treeswift as ts
+from pathlib import Path
+import os
+
+
+# inputs: a placement tree
+# max number of things in each cluster
+from optparse import OptionParser
+from treecluster_sum import min_tree_coloring_sum
+
+
+def aggregate_placements(index_to_node_map, placements):
+    for placement in placements:
+        for i, seqname in enumerate(placement['n']):
+            index = placement["p"][i][0]
+            index_to_node_map[index].placements += [seqname]
+
+
+def set_closest_three_directions(tree):
+    for node in tree.traverse_postorder():
+        if node.is_leaf():
+            node.closest_left = (0, node)
+            node.closest_right = (0, node)
+        else:
+            left, right = node.children
+            lb, ln = left.closest_child
+            node.closest_left = (lb + left.edge_length, ln)
+            rb, rn = right.closest_child
+            node.closest_right = (rb + right.edge_length, rn)
+        if node.closest_left[0] < node.closest_right[0]:
+            node.closest_child = node.closest_left
+        else:
+            node.closest_child = node.closest_right
+
+    for node in tree.traverse_preorder():
+        if node == tree.root:
+            node.closest_top = (float("inf"), None)
+        else:
+            sibling = [c for c in node.parent.children if c != node][0]
+            sb, sn = sibling.closest_child
+            pb, pn = node.parent.closest_top
+            if pb < sibling.edge_length + sb:
+                node.closest_top = (pb + node.edge_length, pn)
+            else:
+                node.closest_top = (sb + sibling.edge_length + node.edge_length, sn)
+
+
+def undo_resolve_polytomies(tree):
+    for e in tree.traverse_postorder():
+        if e == tree.root:
+            continue
+        if e.outgroup == False and e.resolved_randomly:  # edge length check just for safety
+            par = e.parent
+            par.remove_child(e)
+            for c in e.children:
+                par.add_child(c)
+
+
+
+if __name__ == "__main__":
+    parser = OptionParser()
+    parser.add_option("-j", "--jplace", dest="jplace_fp",
+                      help="path to the jplace placement file", metavar="FILE")
+    parser.add_option("-f", "--threshold", dest="threshold", default="600",
+                      help="maximum number of elements in each cluster")
+    parser.add_option("-o", "--output", dest="output_fp",
+                      help="path for the output directory where files will be placed",
+                      metavar="DIRECTORY")
+    parser.add_option("-x", "--extendedref", dest="extended_ref_fp",
+                      help="path to the extened reference alignment file (FASTA), "
+                           "containing reference and query sequences",
+                      metavar="FILE")
+    parser.add_option("-p", "--protein", dest="protein_seqs", action='store_true', default=False,
+                      help="input sequences are protein sequences")
+
+    (options, args) = parser.parse_args()
+
+    with open(options.jplace_fp) as f:
+        jp = json.load(f)
+    tstree = read_tree_newick(jp["tree"])
+
+    index_to_node_map = {}
+    for e in tstree.traverse_postorder():
+        e.placements = []
+        if e != tstree.root:
+            index_to_node_map[e.edge_index] = e
+    aggregate_placements(index_to_node_map, jp["placements"])
+
+    min_tree_coloring_sum(tstree, float(options.threshold))
+
+    set_closest_three_directions(tstree)
+
+    colors = {}
+    for n in tstree.traverse_postorder():
+        if n.color not in colors:
+            colors[n.color] = [n]
+        else:
+            colors[n.color] += [n]
+
+    color_spanning_tree = ts.Tree()
+    r = color_spanning_tree.root
+    r.color = tstree.root.color
+    r.label = str(r.color)
+
+    color_to_node_map = {r.color: r}
+    for n in tstree.traverse_preorder():
+        if n.is_leaf():
+            continue
+        for c in n.children:
+            if c.color != n.color:
+                par = color_to_node_map[n.color]
+                child = ts.Node()
+                par.add_child(child)
+                color_to_node_map[c.color] = child
+                child.joint = n
+                child.color = c.color
+                child.label = str(child.color)
+
+    copyts = tstree.extract_tree_with(labels=tstree.labels())
+    for n in copyts.traverse_preorder():
+        n.outgroup = False
+
+    traversal = list(zip(tstree.traverse_preorder(), copyts.traverse_preorder()))
+    tree_catalog = {}
+
+    for n, ncopy in traversal:
+        ncopy.resolved_randomly = n.resolved_randomly
+        ncopy.placements = n.placements
+        if n.is_leaf():
+            continue
+        cl, cr = n.children
+        clcopy, crcopy = ncopy.children
+
+        if cl.color != n.color:
+            # replace it with representative
+            ncopy.remove_child(clcopy)
+            clcopy_repr = ts.Node()
+            clcopy_repr.outgroup = True
+            lb, ln = n.closest_left
+            clcopy_repr.label = ln.label
+            clcopy_repr.edge_length = lb
+            ncopy.add_child(clcopy_repr)
+
+            # make child a new tree
+            newTree = ts.Tree()
+            newTree.is_rooted = False
+            newTree.root.outgroup = False
+            newTree.root.add_child(clcopy)
+            pb, pn = n.closest_top
+            if pn:
+                top_repr = ts.Node()
+                top_repr.outgroup = True
+                top_repr.label = pn.label
+                top_repr.edge_length = pb
+                newTree.root.add_child(top_repr)
+
+            crcopy_repr = ts.Node()
+            crcopy_repr.outgroup = True
+            rb, rn = n.closest_right
+            crcopy_repr.label = rn.label
+            crcopy_repr.edge_length = rb
+
+            if cl.color == cr.color:
+                ncopy.remove_child(crcopy)
+                ncopy.add_child(crcopy_repr)
+                newTree.root.add_child(crcopy)
+
+            else: # cr.color != n.color and cr.color != cl.color:
+                newTree.root.add_child(crcopy_repr)
+
+            tree_catalog[cl.color] = newTree
+
+        if cr.color != n.color and cr.color != cl.color:
+            ncopy.remove_child(crcopy)
+            crcopy_repr = ts.Node()
+            crcopy_repr.outgroup = True
+            rb, rn = n.closest_right
+            crcopy_repr.label = rn.label
+            crcopy_repr.edge_length = rb
+            ncopy.add_child(crcopy_repr)
+
+            # make child a new tree
+            newTree = ts.Tree()
+            newTree.is_rooted = False
+            newTree.root.outgroup = False
+            newTree.root.add_child(crcopy)
+            pb, pn = n.closest_top
+            if pn:
+                top_repr = ts.Node()
+                top_repr.outgroup = True
+                top_repr.label = pn.label
+                top_repr.edge_length = pb
+                newTree.root.add_child(top_repr)
+
+            clcopy_repr = ts.Node()
+            clcopy_repr.outgroup = True
+            lb, ln = n.closest_left
+            clcopy_repr.label = ln.label
+            clcopy_repr.edge_length = lb
+            newTree.root.add_child(clcopy_repr)
+            tree_catalog[cr.color] = newTree
+
+
+
+    # stitching algorithm:
+    # preorder traversal color_to_node_map
+    # for each node n, find the joint j in tstree.
+    # find LCA of j.left_closest_child and j.right_closest_child in n
+    # replace it with n.children
+
+    Path(options.output_fp).mkdir(parents=True, exist_ok=True)
+
+    fa_dict = fasta2dic(options.extended_ref_fp, options.protein_seqs, False)
+
+    for i, j in tree_catalog.items():
+        partition_output_dir = os.path.join(options.output_fp, str(i))
+        Path(partition_output_dir).mkdir(parents=True, exist_ok=True)
+        undo_resolve_polytomies(j)
+        newick_path = os.path.join(partition_output_dir, "constraint_tree.nwk")
+        j.write_tree_newick(newick_path)
+
+        bipartition_path = os.path.join(partition_output_dir, "bipartition.fasta")
+        with open(bipartition_path, "w") as f:
+            f.write(compute_bipartition_alignment(newick_path))
+
+        species_list_path = os.path.join(partition_output_dir, "species.txt")
+        species_list = []
+        with open(species_list_path, "w") as f:
+            for n in j.traverse_postorder():
+                if n.is_leaf():
+                    species_list += [n.label]
+                    f.write(n.label + "\n")
+                if hasattr(n, 'placements'):
+                    for p in n.placements:
+                        species_list += [p]
+                        f.write(p + "\n")
+
+        alignment_path = os.path.join(partition_output_dir, "alignment.fasta")
+        with open(alignment_path, "w", buffering=100000000) as f:
+            for s in species_list:
+                f.write(">" + s + "\n")
+                f.write(fa_dict[s] + "\n")
+
+
+
+    for i, j in tree_catalog.items():
+        count = 0
+        for n in j.traverse_postorder():
+            try:
+                if n.is_leaf():
+                    count +=1
+                elif n.outgroup is False and hasattr(n, 'placements'):
+                    count += len(n.placements)
+            except:
+                pass
+        print (i, count)
+
+
+
+    print("hello")
