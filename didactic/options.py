dabca10d9a47633b5eae7ac00f926c0cf2029292
metin <metin@metins-MBP.san.rr.com>
2020-11-07 22:12:44 -0800

    runs astral

--- didactic/options.py
+++ didactic/options.py
@@ -1,45 +1,80 @@
-from optparse import OptionParser
+import argparse
+import sys
 from multiprocessing import cpu_count
 from os.path import abspath, expanduser
 from sys import stderr
+from didactic.refine import refine
+from didactic.stitch import stitch
 
 
 def options_config():
-    parser = OptionParser()
-    parser.add_option("-j", "--jplace", dest="jplace_fp",
-                      help="path to the jplace placement file", metavar="FILE")
-    parser.add_option("-f", "--threshold", dest="threshold", default="600",
-                      help="maximum number of elements in each cluster")
-    parser.add_option("-o", "--output", dest="output_fp",
-                      help="path for the output directory where files will be placed",
-                      metavar="DIRECTORY")
-    parser.add_option("-s", "--alignment-dir", dest="alignment_dir_fp",
-                      help="path for input directory which contains "
-                           "extended reference alignment files (FASTA), "
-                           "containing reference and query sequences.",
-                      metavar="FILE")
-    parser.add_option("-p", "--protein", dest="protein_seqs", action='store_true', default=False,
-                      help="input sequences are protein sequences")
-    parser.add_option("-T", "--threads", dest="num_thread", default="0",
-                      help="number of cores used in placement. "
-                           "0 to use all cores in the running machine", metavar="NUMBER")
-    parser.add_option("-l", "--overlap", dest="overlap_length", default="50",
-                      help="minimum alignment overlap length needed to use the subalignment"
-                           "in subtree refinement", metavar="NUMBER")
-    parser.add_option("-c", "--constrain-outgroups", dest="constrain_outgroups", action='store_true', default=False,
-                      help="enforce outgroup topology on gene tree estimation stage.")
-    parser.add_option("-n", "--numtasks", dest="num_tasks", metavar='NUMBER', default="1",
-                      help="number of tasks where local refinement jobs will be split.")
+    # Input arguments parser
+    parser = argparse.ArgumentParser(description='Massively scalable divide-and-conquer phylogenetic inference',
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('-v', '--version', action='store_true', help='print the current version')
+    # parser.add_argument('--debug', action='store_true', help='Print the traceback when an exception is raised')
+    subparsers = parser.add_subparsers(title='commands',
+                                       description='refine   Create local refinement tasks\n'
+                                                   'stitch   Stitch back locally refined trees',
+                                       help='Run run_didactic.py {commands} [-h] for additional help',
+                                       dest='{commands}')
 
-    options, args = parser.parse_args()
+    # To make sure that subcommand is required in python >= 3.3
+    python_version = sys.version_info
+    if (python_version[0] * 10 + python_version[1]) >= 33:
+        subparsers.required = True
 
-    options.num_thread = int(options.num_thread)
-    options.overlap_length = int(options.overlap_length)
-    options.num_tasks = int(options.num_tasks)
-    if not options.num_thread:
+    # Refine command subparser
+    parser_ref = subparsers.add_parser('refine',
+                                       description='Create local refinement tasks')
+    parser_ref.add_argument("-j", "--jplace", dest="jplace_fp",
+                            help="path to the jplace placement file", metavar="FILE")
+    parser_ref.add_argument("-f", "--threshold", dest="threshold", default="600",
+                            help="maximum number of elements in each cluster")
+    parser_ref.add_argument("-o", "--output", dest="output_fp",
+                            help="path for the output directory where files will be placed",
+                            metavar="DIRECTORY")
+    parser_ref.add_argument("-s", "--alignment-dir", dest="alignment_dir_fp",
+                            help="path for input directory which contains "
+                                 "extended reference alignment files (FASTA), "
+                                 "containing reference and query sequences.",
+                            metavar="FILE")
+    parser_ref.add_argument("-p", "--protein", dest="protein_seqs", action='store_true', default=False,
+                            help="input sequences are protein sequences")
+    parser_ref.add_argument("-T", "--threads", type=int, dest="num_thread", default=0,
+                            help="number of cores used in placement. "
+                                 "0 to use all cores in the running machine", metavar="NUMBER")
+    parser_ref.add_argument("-l", "--overlap", type=int, dest="overlap_length", default=50,
+                            help="minimum alignment overlap length needed to use the subalignment"
+                                 "in subtree refinement", metavar="NUMBER")
+    parser_ref.add_argument("-c", "--constrain-outgroups", dest="constrain_outgroups", action='store_true',
+                            default=False,
+                            help="enforce outgroup topology on gene tree estimation stage.")
+    parser_ref.add_argument("-n", "--numtasks", type=int, dest="num_tasks", metavar='NUMBER', default=1,
+                            help="number of tasks where local refinement jobs will be split.")
+    parser_ref.add_argument("-C", "--occupancy", type=float, dest="occupancy_threshold", default=0.66,
+                            help="minimum fraction of species needed to call a gene. "
+                                 "Must be a value between 0 and 1."
+                                 "higly occupant.")
+
+    parser_ref.set_defaults(func=refine)
+
+    # Stitch command subparser
+    parser_sti = subparsers.add_parser('stitch',
+                                       description='Stitch back locally refined trees')
+    parser_sti.add_argument("-o", "--output", dest="output_fp",
+                            help="path for the output directory where files will be placed",
+                            metavar="DIRECTORY")
+    parser_sti.add_argument("-T", "--threads", type=int, dest="num_thread", default=0,
+                            help="number of cores used in placement. "
+                                 "0 to use all cores in the running machine", metavar="NUMBER")
+
+    parser_sti.set_defaults(func=stitch)
+
+    options = parser.parse_args()
+
+    if options.num_thread == 0:
         options.num_thread = cpu_count()
     options.output_fp = abspath(expanduser(options.output_fp))
-    if options.num_tasks < 1:
-        stderr.write("Invalid number of tasks. Number of tasks is set to the minimum value: 1.\n")
-        options.num_tasks = 1
-    return (options, args)
+
+    return options
