b63301bc07a68e7118935f30238abb6d54e941aa
Metin Balaban <balaban@ucsd.edu>
2021-11-29 21:46:50 -0800

    installation script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# create a conda environment including the tools benchmarked.
+if conda info --envs | grep "udance" > /dev/null; then
+        echo "conda environment udance exists"
+else
+        mamba create -y -c bioconda -c conda-forge --channel smirarab --name udance python=3.8 pip newick_utils=1.6 setuptools seqkit==0.16.1 scipy dendropy==4.5.2 pandas==1.3.0 snakemake
+        source activate udance
+        conda activate udance
+        pip install apples==2.0.5
+fi
