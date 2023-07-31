804d6762ecc8b24c814c2551ce2c0ffba8f304c3
Metin Balaban <mb7405@spin.cs.princeton.edu>
2023-07-31 16:54:13 -0400

    now requires python3.9

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ export SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd
 if conda info --envs | grep "udance" > /dev/null; then
         echo "conda environment udance exists"
 else
-	basecomm=$(echo conda create -y -c bioconda -c conda-forge --channel smirarab --name udance python=3.8  pip newick_utils=1.6 setuptools seqkit=2.1.0 scipy dendropy=4.5.2 pandas=1.3.0 snakemake raxml=8.2.12 iqtree=2.1.2 treeshrink=1.3.9 fasttree=2.1.10 julia=1.7.1 gappa=0.7.1 trimal=1.4.1) 
+	basecomm=$(echo conda create -y -c bioconda -c conda-forge --channel smirarab --name udance python=3.9  pip newick_utils=1.6 setuptools seqkit=2.1.0 scipy dendropy=4.5.2 pandas=1.3.0 snakemake raxml=8.2.12 iqtree=2.1.2 treeshrink=1.3.9 fasttree=2.1.10 julia=1.7.1 gappa=0.7.1 trimal=1.4.1) 
 	condaplat=$(conda info | grep "platform" | awk '{print $3}')
 	if [ "$condaplat" == "osx-arm64" ] ; then
 		echo "Installing conda environment on Mac OS X Apple Chip platform"
@@ -32,6 +32,7 @@ else
         julia $SCRIPTS_DIR/uDance/correction_multi.jl datasmall/alignments/p0309.fasta > /dev/null
         # install astral
         pushd $SCRIPTS_DIR/uDance/tools/ASTRAL
-       ./make.sh
+       	./make.sh > .astralInstallation.log 2>&1
+	java -D"java.library.path=lib/" -jar native_library_tester.jar
         popd
 fi
